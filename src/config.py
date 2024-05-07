8e6bb2c5bb50f1361a0153ca644ea349f67b5daf
demamaximmaxim <demamaximmaxim@ya.ru>
2024-05-07 19:14:19 +0300

    clear repo

--- src/config.py
+++
@@ -1,9 +0,0 @@
-SEND_FROM_EMAIL: str = "ks54way@gmail.com" 
-PASSWORD_EMAIL: str = "ewxm wbxy uzqc zipz" 
-
-APK_NAME: str = ''
-
-
-DB_PASSWORD: str = "SOON..."
-DB_NAME: str = "SOON..."
-SQLALCHEMY_DATABASE_URL: str = "sqlite:///tempdatabase.db"
--- src/main.py
+++
@@ -1,9 +0,0 @@
-from server import Server
-
-
-if __name__ == "__main__":
-    
-    server = Server("0.0.0.0", 8090)
-    server.init_routes()
-    
-    server.run()
