cc7ad394ab6de2012ede55f44650143e5e2aa703
demamaximmaxim <demamaximmaxim@ya.ru>
2024-05-07 22:27:12 +0300

    update error handler

--- src/utils/error_handler.py
+++ src/utils/error_handler.py
@@ -5,5 +5,5 @@ from fastapi.encoders import jsonable_encoder
 async def handle_route_error(e: Exception, status_code: status):
     raise HTTPException(
         detail=jsonable_encoder(e),
-        status_code=status.HTTP_400_BAD_REQUEST
+        status_code=status_code
     )
