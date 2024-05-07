24c86227faee48e4b0f68b969932dd505238d93a
demamaximmaxim <demamaximmaxim@ya.ru>
2024-05-07 20:13:44 +0300

    commit

--- src/api/users.py
+++ src/api/users.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, HTTPException, status
 from fastapi.encoders import jsonable_encoder
 
 from repositories.users import UsersRepository
@@ -16,6 +16,13 @@ async def add_user(
     user: UserRequest,
 ):
     user_dict = user.model_dump()
-    new_user = await UsersRepository().add_one(user_dict)
-    return new_user 
+    try:
+        new_user = await UsersRepository().add_one(user_dict)
+        return new_user
+    
+    except Exception as e:
+        raise HTTPException(
+            detail=jsonable_encoder(e),
+            status_code=status.HTTP_400_BAD_REQUEST
+        ) 
 
