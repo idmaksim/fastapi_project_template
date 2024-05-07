bfb02ada87b0e9ac9debd1c24335bde36935854c
demamaximmaxim <demamaximmaxim@ya.ru>
2024-05-07 22:17:41 +0300

    add test username

--- src/schemas/users.py
+++ src/schemas/users.py
@@ -5,6 +5,7 @@ class UserRequest(BaseModel):
     email: str = Field(max_length=40, min_length=5)
     password: str = Field(max_length=30, min_length=6)
     name: str = Field(max_length=30, min_length=2)
+    username: str
 
 
 class UserRead(UserRequest):
