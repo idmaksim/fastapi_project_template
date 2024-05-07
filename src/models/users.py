07cf4af03f098631e2bca847140fadb72886a800
demamaximmaxim <demamaximmaxim@ya.ru>
2024-05-07 19:22:30 +0300

    add new files

---
+++ src/models/users.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String
+from ..db.db import Base
+
+
+class Users(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, unique=True, autoincrement=True, nullable=False)
+    email: str = Column(String, unique=True, nullable=False)
+    password: str = Column(String, nullable=False)
+    name: str = Column(String, nullable=False)
---
+++ src/schemas/users.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel, Field
+
+
+class UserRequest(BaseModel):
+    email: str = Field(max_length=40, min_length=5)
+    password: str = Field(max_length=30, min_length=6)
+    name: str = Field(max_length=30, min_length=2)
+    gender_id: int = Field(gt=0, default=1)
