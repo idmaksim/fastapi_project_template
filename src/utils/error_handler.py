b7107665b0faf16ea0757a1d62b6b83d515268b7
demamaximmaxim <demamaximmaxim@ya.ru>
2024-05-07 22:19:22 +0300

    error handler fix

--- src/utils/error_handler.py
+++ src/utils/error_handler.py
@@ -1,6 +1,7 @@
 from fastapi import HTTPException, status
 from fastapi.encoders import jsonable_encoder
 
+
 async def handle_route_error(e: Exception, status_code: status):
     raise HTTPException(
         detail=jsonable_encoder(e),
