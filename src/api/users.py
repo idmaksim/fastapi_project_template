3b6275cfaab763e154eb8864f3d37d98a55d4c57
idmaksim <i@dmaksim.ru>
2024-05-11 00:30:01 +0300

    commit

--- src/api/users.py
+++ src/api/users.py
@@ -105,5 +105,5 @@ async def get_all_users(
         )
         
     except Exception as e:
-        await handle_route_error(e, status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        await handle_route_error(e, status_code=status.HTTP_404_NOT_FOUND)
 
