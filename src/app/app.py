de2e8ed9c244196dfb7d5a320be904937ac22383
idmaksim <i@dmaksim.ru>
2024-05-11 18:30:57 +0300

    add redirect response

--- src/app/app.py
+++ src/app/app.py
@@ -1,5 +1,6 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 
 from db.db import create_db_and_tables
 from api.routers import main_api_router
@@ -13,6 +14,11 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan, include_in_schema=True)
 app.include_router(main_api_router)
+
+
+@app.get('/', response_class=RedirectResponse)
+async def redirect_to_docs():
+    return '/docs'
 
 
 
