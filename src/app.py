ff40a2b6542fbe7700baf9b9df2716dd09c03c8e
idmaksim <i@dmaksim.ru>
2024-06-27 14:26:36 +0300

    add cors middleware

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 from db.db import create_db_and_tables
 from views.routers import main_api_router
@@ -13,6 +14,21 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(main_api_router)
 
 
